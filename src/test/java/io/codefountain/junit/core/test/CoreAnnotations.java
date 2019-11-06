41c978301d4faa33dedb28cbe8f403f603067394
musibs <musib.somnath@gmail.com>
2019-11-07 08:31:28 +1100

    Junit Annotations

--- src/test/java/io/codefountain/junit/core/test/CoreAnnotations.java
+++ src/test/java/io/codefountain/junit/core/test/CoreAnnotations.java
@@ -1,9 +1,16 @@
 package io.codefountain.junit.core.test;
 
 import org.junit.*;
+import org.junit.rules.Timeout;
+
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.Assert.assertEquals;
 
 public class CoreAnnotations {
+
+    //@Rule
+    //public Timeout timeOut = Timeout.seconds(5);
 
     @BeforeClass
     public static void runBeforeClass(){
@@ -34,12 +41,23 @@ public class CoreAnnotations {
 
 
     @Test
+    @Ignore
     public void testCase2(){
         System.out.println("Running test case 2");
         assertEquals(4, 2*2);
     }
 
-    @Test
+    @Test(timeout = 3000)
+    //@Test
+    public void testCase3() throws InterruptedException {
+        System.out.println("Running test case 3");
+        assertEquals(4, 2*2);
+        System.out.println(Thread.currentThread().getName());
+        TimeUnit.SECONDS.sleep(10);
+    }
+
+
+    @Test(expected = ArithmeticException.class)
     public void expectedException(){
         int a = 1/0;
     }
---
+++ src/test/java/io/codefountain/junit/core/test/ProductTestSuite.java
@@ -0,0 +1,10 @@
+package io.codefountain.junit.core.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({CoreAnnotations.class, RuleAnnotationTest.class})
+public class ProductTestSuite {
+
+}
